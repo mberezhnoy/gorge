9a875329413c9b5a410d7bf1298388c4ec52bd29
ww-github <info@whitewater.guide>
2020-02-15 19:40:30 +0300

    build: copy

--- .github/actions/libproj/entrypoint.sh
+++ .github/actions/libproj/entrypoint.sh
@@ -1,9 +1,4 @@
 #!/bin/sh -l
 
-printenv
-ls -la /github
-ls -la /github/home
-ls -la /github/workspace
-
-mkdir -p /github/workspace/libproj
-cp /usr/lib/libproj.*a /github/workspace/libproj/
+mkdir -p ${GITHUB_WORKSPACE}/libproj
+cp /usr/lib/libproj.*a ${GITHUB_WORKSPACE}/libproj/
